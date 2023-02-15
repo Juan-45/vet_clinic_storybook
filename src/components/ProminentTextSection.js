2299a6a22a7a9a9cd968febb3b9b77fe97e00cf9
black_wolf <62311128+Juan-45@users.noreply.github.com>
2023-02-15 17:37:40 -0300

    Create ProminentTextSection component: (#8)
    - Includes separated Styles file
    - Includes story
    - Includes asset

---
+++ src/components/ProminentTextSection.js
@@ -0,0 +1,7 @@
+import { Container } from "components/prominentTextSection/Styles";
+
+const ProminentTextSection = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default ProminentTextSection;
