ec718a04b4d07ca686dcb96cab94f2a0ac31da89
Juan-45 <quijuan12@gmail.com>
2023-02-16 12:17:42 -0300

    Add bounceIn animation:
    - The animation is fired when the container is fully in the screen
    - Includes differents delay accordingly to the amount of row items

--- src/components/MainServices.js
+++ src/components/MainServices.js
@@ -3,11 +3,14 @@ import { LargeContainer, FlexRowCenter } from "components/CommonStyles";
 import { HighlightRed } from "components/CommonStyles";
 import { Box, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { keyframes } from "@mui/styled-engine";
+import { useMediaQuery } from "@mui/material";
 import mergician from "mergician";
 import vaccination from "stories/assets/vaccination.png";
 import toothbrush from "stories/assets/toothbrush.png";
 import bed from "stories/assets/bed.png";
 import ambulance from "stories/assets/ambulance.png";
+import useTriggerOnScroll from "hooks/useTriggerOnScroll";
 
 const Container = styled(LargeContainer)({
   display: "flex",
@@ -23,7 +26,29 @@ const ServicesListContainer = styled(Box)(({ theme }) => ({
   flexWrap: "wrap",
 }));
 
-const InfoContainer = styled(Box)(({ theme }) => ({
+const ANIMATION_TIME = 0.8;
+
+const bounceIn = keyframes({
+  "0%": {
+    opacity: "0",
+    transform: "scale(.3)",
+  },
+  "50%": {
+    opacity: "1",
+    transform: "scale(1.05)",
+  },
+  "70%": {
+    transform: "scale(.9)",
+  },
+  "100%": {
+    transform: "scale(1)",
+    opacity: "1",
+  },
+});
+
+const InfoContainer = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "trigger" && prop !== "delay",
+})(({ theme, trigger, delay }) => ({
   minHeight: "300px",
   background: theme.palette.background.default,
   display: "flex",
@@ -42,6 +67,10 @@ const InfoContainer = styled(Box)(({ theme }) => ({
   [theme.breakpoints.up("tablet_min_900")]: {
     maxWidth: "calc(25% - 16px)",
   },
+  opacity: 0,
+  animation: trigger
+    ? `${ANIMATION_TIME}s ease ${delay}s forwards ${bounceIn}`
+    : "unset",
 }));
 
 const StyledImg = styled("img")(({ theme }) => ({
@@ -73,10 +102,38 @@ const Description = styled(Typography)(({ theme }) =>
 );
 
 const MainServices = () => {
+  const { targetEl: targetEl1, trigger: trigger1 } = useTriggerOnScroll();
+  const { targetEl: targetEl2, trigger: trigger2 } = useTriggerOnScroll();
+  const { targetEl: targetEl3, trigger: trigger3 } = useTriggerOnScroll();
+  const { targetEl: targetEl4, trigger: trigger4 } = useTriggerOnScroll();
+
+  const between600_899 = useMediaQuery(
+    "(min-width: 600px) and (max-width:899px)"
+  );
+
+  const max599 = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+  const getDelayArr = ({ max599, between600_899 }) => {
+    if (max599) {
+      const results = [0, 0, 0, 0];
+      return results;
+    } else if (between600_899) {
+      const results = [0, 0.3, 0, 0.3];
+      return results;
+    } else {
+      const results = [0, 0.3, 0.6, 0.9];
+      return results;
+    }
+  };
+
   return (
     <Container>
       <ServicesListContainer>
-        <InfoContainer>
+        <InfoContainer
+          ref={targetEl1}
+          trigger={trigger1}
+          delay={getDelayArr({ max599, between600_899 })[0]}
+        >
           <StyledImg src={vaccination} alt='vaccination' />
           <Title variant='h2'>Vacunación.</Title>
           <Description>
@@ -84,7 +141,11 @@ const MainServices = () => {
             futuro saludable comienza con una prevención efectiva.
           </Description>
         </InfoContainer>
-        <InfoContainer>
+        <InfoContainer
+          ref={targetEl2}
+          trigger={trigger2}
+          delay={getDelayArr({ max599, between600_899 })[1]}
+        >
           <StyledImg src={toothbrush} alt='dental health' />
           <Title variant='h2'>Salud dental.</Title>
           <Description>
@@ -92,7 +153,11 @@ const MainServices = () => {
             una vida larga y feliz.
           </Description>
         </InfoContainer>
-        <InfoContainer>
+        <InfoContainer
+          ref={targetEl3}
+          trigger={trigger3}
+          delay={getDelayArr({ max599, between600_899 })[2]}
+        >
           <StyledImg src={bed} alt='admission' />
           <Title variant='h2'>Internaciones.</Title>
           <Description>
@@ -100,7 +165,11 @@ const MainServices = () => {
             mascota estará en buenas manos durante su recuperación.
           </Description>
         </InfoContainer>
-        <InfoContainer>
+        <InfoContainer
+          ref={targetEl4}
+          trigger={trigger4}
+          delay={getDelayArr({ max599, between600_899 })[3]}
+        >
           <StyledImg src={ambulance} alt='emergency services 24 hours' />
           <Title variant='h2'>Servicio de emergencias 24 hs.</Title>
           <Description>
